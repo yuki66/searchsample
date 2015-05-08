8ceda1766ec8377e6e50a427e8f9e1e8a2153c29
yuki66 <nktiga6@gmail.com>
2015-05-08 21:56:04 +0900

    Initial Commit

---
+++ search test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  search test
+//
+//  Created by YUKIKO HARADA on 2015/05/08.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ search test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  search test
+//
+//  Created by YUKIKO HARADA on 2015/05/08.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
